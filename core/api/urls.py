18a393f4319bdaf6265d0d8d10046020c5b9b320
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-01-30 23:27:43 +0100

    change app to api to make it more clear

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import ItemListCreateView
+from api.views import ItemListCreateView
 
 urlpatterns = [
     # path('', views.index, name='index'),
--- core/core/settings.py
+++ core/core/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'rest_framework',
-    'blog',
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
