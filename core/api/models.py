c2409bc234016630554c1acb1814ba52a8bc7b8a
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-02-09 12:35:52 +0100

    chore:add image url feature

--- core/api/models.py
+++ core/api/models.py
@@ -14,6 +14,7 @@ class Author(models.Model):
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    post_image_url = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         Author, on_delete=models.CASCADE, null=True, related_name='blog_posts', default=1)
