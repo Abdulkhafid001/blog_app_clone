0396b7e3b23e5313660158680db988fa468b30b7
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-02-14 18:40:11 +0100

    add signals for model relationships

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -6,7 +6,8 @@ from .models import *
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = '__all__'
+        # fields = '__all__'
+        fields = [''] 
 
 
 class CommentSerializer(serializers.ModelSerializer):
