2c8dc412e17cbb8331d637464a48b74d796fd5d4
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-02-15 17:24:51 +0100

    chore:add signup frontend

--- core/api/urls.py
+++ core/api/urls.py
@@ -4,8 +4,8 @@ from api.views import *
 urlpatterns = [
     # path("api/blogposts/", BlogPostListCreate.as_view(), name="blogpost-view-create"),
     # path("api/blogposts/<int:pk>", BlogPostRetrieveUpdateDestroy.as_view(), name="update"),
-    # path("api/authors/", AuthorListCreate.as_view(), name="blogpost-view-create"),
-    # path("api/authors/<int:pk>", AuthorRetrieveUpdateDestroy.as_view(), name="update"),
+    path("api/authors/", AuthorListCreate.as_view(), name="blogpost-view-create"),
+    path("api/authors/<int:pk>", AuthorRetrieveUpdateDestroy.as_view(), name="update"),
 
     # custom URL routes
     path("blogposts/",  BlogPostCreateViewFrontend.as_view(), name="blog-post-create"),
